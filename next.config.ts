090fd3221f946fabd4c22e3abfd3a15a904f456d
yvann <yvann.guyonnet@gmail.com>
2025-07-25 17:36:59 +0200

    change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export", // <- commente ou supprime cette ligne
   images: {
     unoptimized: true,
   },
